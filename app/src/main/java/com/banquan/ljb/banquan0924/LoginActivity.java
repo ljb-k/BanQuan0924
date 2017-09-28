c4f7a8e7ec9bb15cca64af68e9641a05fc2f457d
ljb-k <354275172@qq.com>
2017-09-28 20:42:32 +0800

    注册界面

--- app/src/main/java/com/banquan/ljb/banquan0924/LoginActivity.java
+++ app/src/main/java/com/banquan/ljb/banquan0924/LoginActivity.java
@@ -1,13 +1,46 @@
 package com.banquan.ljb.banquan0924;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText editText_accounts;
+    private EditText editText_password;
+    private Button button_login;
+    private TextView textView_register;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        initView();
     }
+
+    private void initView() {
+        editText_accounts = (EditText) findViewById(R.id.accounts);
+        editText_password = (EditText) findViewById(R.id.password);
+        button_login = (Button) findViewById(R.id.login);
+        textView_register = (TextView) findViewById(R.id.register_Text);
+
+        textView_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(LoginActivity.this,RegisterActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+
 }
